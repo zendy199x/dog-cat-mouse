651e13825dd6fe2e965af8607d02b12380d63bac
Zendy's Macbook <zendy@Macbooks-Zendy.local>
2019-05-15 00:07:52 +0700

    Let dog eat Cat

--- Dog.js
+++ Dog.js
@@ -5,3 +5,9 @@ function Dog() {
 Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
 }
+
+Dog.prototype.sayHi = function (arguments) {
+	console.log('Hi! I am a dog. My name is ' + chalk.red(this.name));	
+};
+
+module.exports = Dog;
